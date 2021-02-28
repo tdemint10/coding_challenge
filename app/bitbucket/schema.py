ec259d5d7463999508418c0ffdd800048f33c9ae
Travis DeMint <deminttravis9@gmail.com>
2021-02-28 13:11:41 -0600

    added bitbucket followers request

--- app/bitbucket/schema.py
+++ app/bitbucket/schema.py
@@ -4,6 +4,7 @@ from marshmallow import fields, Schema
 class BitbucketProfileSchema(Schema):
     """ Bitbucket Profile Schema """
 
+    followerCount = fields.Integer(attribute="follower_count")
     repoCount = fields.Integer(attribute="repo_count")
     languages = fields.List(fields.String(), attribute="languages")
     watchersCount = fields.Integer(attribute="watchers_count")
--- app/bitbucket/service.py
+++ app/bitbucket/service.py
@@ -8,6 +8,7 @@ import requests
 BASE_URL = "https://api.bitbucket.org/2.0"
 
 GET_REPOS_ENDPOINT = "repositories/%s?page=%d"
+GET_FOLLOWERS_ENDPOINT = "teams/%s/followers"
 GET_WATCHERS_ENDPOINT = "repositories/%s/%s/watchers"
 
 
@@ -43,7 +44,7 @@ class BitbucketService:
             page += 1
 
 
-            if not "next" in r:
+            if not "next" in r.json():
                 next_page_exists = False
 
         # return result
@@ -51,8 +52,33 @@ class BitbucketService:
 
 
     @staticmethod
+    def get_followers_count(team: str):
+        app.logger.debug(f"BitbucketService - get_followers_count - team: {team}")
+
+        # build url for the needed request
+        request_url = f"{BASE_URL}/{GET_FOLLOWERS_ENDPOINT}" % team
+        app.logger.debug(f"request_url: {request_url}")
+
+        # make request
+        r = requests.get(request_url)
+
+        if r.status_code == 403:
+            app.logger.error("FAILED - Bitbucket Forbidden")
+            raise ForbiddenException(f"FAILED - Access Forbidden to Bitbucket Team: {team}. (Bitbucket API rate limit may be exceeded)")
+        elif not r.status_code == 200:
+            app.logger.error(f"FAILED to get Bitbucket Watchers for: {team}/{repo}")
+            raise Exception(f"FAILURE - request failed - {request_url}")
+
+        # return follower count
+        if "size" in r.json():
+            return r.json()["size"]
+        else:
+            return len(r.json()["values"])
+
+
+    @staticmethod
     def get_watchers_count(team: str, repo: str):
-        app.logger.debug(f"BitbucketService - get_watchers - team: {team}, repo: {repo}")
+        app.logger.debug(f"BitbucketService - get_watchers_count - team: {team}, repo: {repo}")
 
         # build url for the needed request
         request_url = f"{BASE_URL}/{GET_WATCHERS_ENDPOINT}" % (team, repo)
@@ -69,7 +95,10 @@ class BitbucketService:
             raise Exception(f"FAILURE - request failed - {request_url}")
 
         # return watchers
-        return r.json()["size"]
+        if "size" in r.json():
+            return r.json()["size"]
+        else:
+            return len(r.json()["values"])
 
 
     @staticmethod
@@ -83,6 +112,7 @@ class BitbucketService:
 
         # populate profile values
         profile["repo_count"] = len(repos)
+        profile["follower_count"] = BitbucketService.get_followers_count(team)
 
         # update values for each individual repo
         for repo in repos:
@@ -99,6 +129,7 @@ class BitbucketService:
 
         # set default values
         profile["repo_count"] = 0
+        profile["follower_count"] = 0
         profile["languages"] = set({})
         profile["watchers_count"] = 0
 
