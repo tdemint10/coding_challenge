3aa8385113e129c9c2ada9023fd65d736b171733
Travis DeMint <deminttravis9@gmail.com>
2021-02-28 12:06:26 -0600

    handle paginated results

--- app/bitbucket/service.py
+++ app/bitbucket/service.py
@@ -7,7 +7,7 @@ import requests
 # Bitbucket API url and paths for the requests
 BASE_URL = "https://api.bitbucket.org/2.0"
 
-GET_REPOS_ENDPOINT = "repositories/%s"
+GET_REPOS_ENDPOINT = "repositories/%s?page=%d"
 GET_WATCHERS_ENDPOINT = "repositories/%s/%s/watchers"
 
 
@@ -16,22 +16,38 @@ class BitbucketService:
     def get_repos(team: str):
         app.logger.debug(f"BitbucketService - get_repos - team: {team}")
 
-        # build url for the needed request
-        request_url = f"{BASE_URL}/{GET_REPOS_ENDPOINT}" % team
-        app.logger.debug(f"request_url: {request_url}")
+        repos = []
+        page = 1
 
-        # make request
-        r = requests.get(request_url)
+        # track when done looping through pages
+        next_page_exists = True
 
-        if r.status_code == 403:
-            app.logger.error("FAILED - Bitbucket Forbidden")
-            raise ForbiddenException(f"FAILED - Access Forbidden to Bitbucket Team: {team}. (Bitbucket API rate limit may be exceeded)")
-        elif not r.status_code == 200:
-            app.logger.error(f"FAILED to get Bitbucket Repositories for: {team}")
-            raise Exception(f"FAILURE - request failed - {request_url}")
+        # loop through paginated responses
+        while next_page_exists:
+            # build url for the needed request
+            request_url = f"{BASE_URL}/{GET_REPOS_ENDPOINT}" % (team, page)
+            app.logger.debug(f"request_url: {request_url}")
 
-        # return data
-        return r.json()["values"]
+            # make request
+            r = requests.get(request_url)
+
+            if r.status_code == 403:
+                app.logger.error("FAILED - Bitbucket Forbidden")
+                raise ForbiddenException(f"FAILED - Access Forbidden to Bitbucket Team: {team}. (Bitbucket API rate limit may be exceeded)")
+            elif not r.status_code == 200:
+                app.logger.error(f"FAILED to get Bitbucket Repositories for: {team}")
+                raise Exception(f"FAILURE - request failed - {request_url}")
+
+            # append data
+            repos.extend(r.json()["values"])
+            page += 1
+
+
+            if not "next" in r:
+                next_page_exists = False
+
+        # return result
+        return repos
 
 
     @staticmethod
